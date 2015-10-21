3aaea6fd6675b928862a9218682cb726297cd3e0
Sergiusz 'q3k' Bazański <q3k@q3k.org>
2015-10-21 16:38:48 +0800

    Implement basic functionality

---
+++ at.py
@@ -0,0 +1,85 @@
+# Copyright (c) 2015 Sergiusz 'q3k' Bazanski <serigusz@bazanski.pl>
+# This is free and unencumbered software released into the public domain.
+__author__ = "Sergiusz 'q3k' Bazanski"
+
+import hashlib
+import json
+import time
+
+import MySQLdb
+import requests
+
+LEASE_URL = 'http://10.0.10.5/dhcp.leases'
+LEASE_CACHE_TIMEOUT = 10  # seconds
+MYSQL_USER = 'root'
+MYSQL_DB = 'members'
+
+lease_cache_time = 0
+lease_cache_data = []
+def get_leases():
+    """Download and cache leases from router."""
+    global lease_cache_time, lease_cache_data
+
+    # Check cache
+    now = time.time()
+    if now < lease_cache_time + LEASE_CACHE_TIMEOUT:
+        return lease_cache_data
+
+    # Get leases
+    text = requests.get(LEASE_URL).text
+    macs = []
+    for line in text.split('\n'):
+        if not line.strip():
+            continue
+        mac = line.split()[1]
+        macs.append(mac)
+
+    # Update cache
+    lease_cache_data = macs
+    lease_cache_time = now
+    return macs
+
+
+def _connect_db():
+    """Connect to MySQL user database and return a cursor."""
+    db = MySQLdb.connect(user=MYSQL_USER, db=MYSQL_DB)
+    return db.cursor()
+
+
+def _anonymyze_email(email):
+    """Anonymize an email to make spiders piss off."""
+    return email
+
+
+def _hash_mac(mac, salt):
+    h = hashlib.sha1()
+    h.update(salt)
+    h.update(mac)
+    return h.hexdigest()
+
+
+def get_clients(macs):
+    """Transform a list of macs into a client->macs name dictionary."""
+    SALT = 'salT'  # hack! this might not always be the real salt.
+    hashed_macs = dict((_hash_mac(m, SALT), m) for m in macs)
+    sql_macs = ', '.join('"{}"'.format(m) for m in hashed_macs)
+
+    cursor = _connect_db()
+    cursor.execute('SELECT email, mac FROM Users WHERE mac IN (' + sql_macs + ')')
+
+    clients = {}
+    for email, hashed_mac in cursor.fetchall():
+        mac = hashed_macs[hashed_mac]
+        if email not in clients:
+            clients[email] = []
+        clients[email].append(mac)
+    return clients
+
+
+def generate_json():
+    macs = get_leases()
+    clients = get_clients(macs)
+    return json.dumps([_anonymyze_email(e) for e in clients.keys()])
+
+
+print generate_json()
